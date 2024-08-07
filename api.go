bc885e54475ce291eb9003364c5bf4a8194bd56f
Ke Chen <JingYiJun3104@outlook.com>
2024-08-07 11:49:17 +0800

    chore: upgrade dependency

--- api.go
+++ api.go
@@ -1,12 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/opentreehole/go-common"
+)
 
 func GetAuthToken(c *fiber.Ctx) error {
 	return c.SendString("PF.obj.config.auth_token = \"123456789\"")
 }
 
 func UploadImage(c *fiber.Ctx) error {
+	var _, err = common.GetUserID(c)
+	if err != nil {
+		return err
+	}
 	var response CheveretoUploadResponse
 	file, err := c.FormFile("source")
 	if err != nil {
